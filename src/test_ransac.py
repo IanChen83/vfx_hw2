205da59ee93a7a1a4943050d6b19028ed77207c7
Patrick Chen <patrickchen1994@gmail.com>
2016-05-12 09:57:19 +0800

    Ransac

--- src/test_ransac.py
+++ src/test_ransac.py
@@ -9,25 +9,25 @@ import sys
 focal = 300
 #showcase_mask(np.array(list(range(255))))
 #
-# mat1 = parse_mat("../grail/1.mat")
-# mat2 = parse_mat("../grail/2.mat")
+#mat1 = parse_mat("../grail/1.mat")
+#mat2 = parse_mat("../grail/2.mat")
 # mat3 = parse_mat("../grail/3.mat")
-# mat71 = parse_mat("../data/71.mat")
-# mat72 = parse_mat("../data/72.mat")
+#mat71 = parse_mat("../71.mat")
+#mat72 = parse_mat("../72.mat")
 
 merger = Merger()
 
 
-# img2 = cyl_mapping(Image.open("../grail/2.jpg"), 300, mat2).transpose(1, 0, 2)
-# img1 = cyl_mapping(Image.open("../grail/1.jpg"), 300, mat1).transpose(1, 0, 2)
+#img2 = Image.open("../72.jpg")
+#img1 = Image.open("../71.jpg")
 
-# mat12, q12 = match(mat1, mat2)
+#mat12, q12 = match(mat71, mat72)
 # mat23, q23 = match(mat2, mat3)
-# for x in q3:
-#     mark_point(img1, x.fp1.xy())
-#     mark_point(img2, x.fp2.xy())
-# img1.show()
-# img2.show()
+#for x in q12:
+#    mark_point(img1, x.fp1.xy(), 30)
+#    mark_point(img2, x.fp2.xy(), 30)
+#img1.save("data_1.jpg")
+#img2.save("data_2.jpg")
 #showcase_pairing(img1, img2, q3).show()
 #
 # mat69_70, queue69_70 = match(mat69, mat70)
@@ -59,5 +59,5 @@ for i in range(start, end):
     if i > start:
         merger.add_matrix(match(matrices[i-1], matrices[i])[0], str(i-1), str(i))
 
-merger.merge("0").show()
+merger.merge("0").save("grail{focal}.jpg".format(focal=focal))
 
